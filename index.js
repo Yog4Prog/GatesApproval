d0dc5d27eaba49514051e28b5196832b16bf1b90
Yog4Prog <raju4techno@gmail.com>
2022-10-18 19:53:46 +0530

    Updated secrets

--- index.js
+++ index.js
@@ -12,41 +12,52 @@ const issue_title = core.getInput('issue-title')
 const body_message = core.getInput('body_message')
 const labels = core.getInput('lables')
 
+async function createApprovalIssue() {
+
+    console.log(`owner ${owner}`)
+    console.log(`repo ${repo}`)
+    console.log(`assignees ${assignees}`)
+    console.log(`token ${token}`)
+    console.log(`timeout ${timeout}`)
+    console.log(`issue_title ${issue_title}`)
+    console.log(`body_message ${body_message}`)
+    console.log(`labels ${labels}`)
+    
+    var createIssuePayload = JSON.stringify(
+        {
+            owner: 'Yog4Prog',
+            repo: 'GatesApproval',
+            title: 'A Scan has a failure.. Please approve to proceed',
+            body: 'Found an Issue while performing SCA Scan',
+            assignees: [
+                'Yog4Prog'
+            ],
+            labels: [
+                'bug'
+            ]
+        }
+    );
+
+    var createIssueRequest = {
+        method: 'post',
+        url: 'https://api.github.com/repos/Yog4Prog/GatesApproval/issues',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.github.v3+json'
+        },
+        data: createIssuePayload
+    };
 
 
-var createIssuePayload = JSON.stringify(
-    {
-        owner: 'Yog4Prog',
-        repo: 'GatesApproval',
-        title: 'A Scan has a failure.. Please approve to proceed',
-        body: 'Found an Issue while performing SCA Scan',
-        assignees: [
-            'Yog4Prog'
-        ],
-        labels: [
-            'bug'
-        ]
-      }
-);
+    await axios(createIssueRequest)
+        .then(function (res) {
+            console.log("Github Approval Issue successfully created !!");
+        })
+        .catch(function (error) {
+            console.log("Failed to create an Github Approval Issue." + error)
+        });
+}
 
 
-var createIssueRequest = {
-    method: 'post',
-    url: 'https://api.github.com/repos/Yog4Prog/GatesApproval/issues',
-    headers: {
-        'Authorization': 'Bearer '+token,
-        'Content-Type': 'application/json',
-        'Accept': 'application/vnd.github.v3+json'
-    },
-    data: createIssuePayload
-};
-
-
-axios(createIssueRequest)
-.then( function(res) {
-    console.log("Github Approval Issue successfully created !!");
-})
-.catch( function(error){
-    console.log("Failed to create an Github Approval Issue."+ error)
-});
-   
+createApprovalIssue()
