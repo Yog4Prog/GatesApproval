90f63d9c52a7951c8c5a6a68e8d9734bb973b261
Yog4Prog <raju4techno@gmail.com>
2022-10-18 17:08:40 +0530

    Using axios

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const Octokit = require("@octokit/rest");
+const axios = require("axios");
 
-const octokit = new Octokit({
-    auth: core.getInput('secret')
+
+const token = core.getInput('secret')
+
+var createPayload = JSON.stringify(
+    {
+        owner: 'Yog4Prog',
+        repo: 'GatesApproval',
+        title: 'Approve an Issue',
+        body: 'Found a bug need approval to ontinue',
+        assignees: [
+          'Yog4Prog'
+        ],
+        labels: [
+          'bug'
+        ]
+      }
+);
+
+
+var requestString = {
+    method: 'post',
+    url: 'https://api.github.com/repos/Yog4Prog/GitHubActionsLab1/issues',
+    headers: {
+        'Authorization': 'Bearer '+token,
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.github.v3+json'
+    },
+    data: createPayload
+};
+
+
+axios(requestString)
+.then( function(res) {
+    console.log("Issue successfully created !!");
 })
-
-try {
-
-  octokit.request('POST /repos/Yog4Prog/GatesApproval/issues', {
-    owner: 'Yog4Prog',
-    repo: 'GatesApproval',
-    title: 'Approve an Issue',
-    body: 'Found a bug need approval to ontinue',
-    assignees: [
-      'Yog4Prog'
-    ],
-    labels: [
-      'bug'
-    ]
-  })
-} catch (error) {
-  core.setFailed(error.message);
-}
-
+.catch( function(error){
+    console.log("Failed to created issue "+ error)
+});
+   
