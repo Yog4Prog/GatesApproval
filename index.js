1d7ae29cec4db2c55f8f473995cd0e36544401ce
Yog4Prog <raju4techno@gmail.com>
2022-10-18 15:43:30 +0530

    Added ResTAPI cal lto create an Issue

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const octokit = new Octokit({
+    auth: core.getInput('secret')
+})
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+  await octokit.request('POST /repos/Yog4Prog/GatesApproval/issues', {
+    owner: 'Yog4Prog',
+    repo: 'GatesApproval',
+    title: 'Approve an Issue',
+    body: 'Found a bug need approval to ontinue',
+    assignees: [
+      'Yog4Prog'
+    ],
+    labels: [
+      'bug'
+    ]
+  })
 } catch (error) {
   core.setFailed(error.message);
 }
+
