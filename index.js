1d1d38df9cc66d07addee799b4344ab33dd67877
Yog4Prog <raju4techno@gmail.com>
2022-10-18 18:04:12 +0530

    Updated secrets

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ const github = require('@actions/github');
 const axios = require("axios");
 
 
+const owner = core.getInput('owner')
+const repo = core.getInput('repo')
+const assignees = core.getInput('approvers')
 const token = core.getInput('secret')
+const timeout = core.getInput('timeout')
+const issue_title = core.getInput('issue-title')
+const body_message = core.getInput('body_message')
+const labels = core.getInput('lables')
 
 var createPayload = JSON.stringify(
     {
-        owner: 'Yog4Prog',
-        repo: 'GatesApproval',
-        title: 'Approve an Issue 5',
-        body: 'Found a bug need approval to ontinue',
+        owner: owner,
+        repo: repo,
+        title: issue_title,
+        body: body_message,
         assignees: [
-          'Yog4Prog'
+            assignees
         ],
         labels: [
-          'bug'
+            labels
         ]
       }
 );
@@ -23,9 +30,9 @@ var createPayload = JSON.stringify(
 
 var requestString = {
     method: 'post',
-    url: 'https://api.github.com/repos/Yog4Prog/GatesApproval/issues',
+    url: `https://api.github.com/repos/${owner}/${repo}/issues`,
     headers: {
-        'Authorization': 'Bearer '+token,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
         'Accept': 'application/vnd.github.v3+json'
     },
